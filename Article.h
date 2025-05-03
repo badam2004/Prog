ac2454aa5a875df392d774882289e6721394f342
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-03 11:49:56 +0200

    Update Article.h

--- Article.h
+++ Article.h
@@ -1,3 +1,4 @@
+/*
 #ifndef ARTICLE_H
 #define ARTICLE_H
 
@@ -21,5 +22,29 @@ public:
     std::string getAuthor() const { return author; }
     int getArticleId() const {return ArticleId; }
 };
-
+*/
+//////////////////////////////////////////////////
 #endif //ARTICLE_H
+
+#ifndef ARTICLE_H
+#define ARTICLE_H
+
+#include <string>
+#include "Content.h"
+
+class Article : public Content {
+    std::string title;
+    std::string content;
+    std::string author;
+    int ArticleId = 0; // inicializálás alapból 0-ra, ha nincs konstruktorban
+public:
+    Article(std::string t, std::string c, std::string a);
+
+    void display() const override;
+    std::string getType() const override;
+
+    std::string getAuthor() const;
+    int getArticleId() const;
+};
+
+#endif // ARTICLE_H

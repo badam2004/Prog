9c14f8abac8e4e2e956c0c70003f6fe815043593
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-04 16:12:53 +0200

    Update Comment.cpp

--- Comment.cpp
+++ Comment.cpp
@@ -8,7 +8,7 @@ Comment::Comment(std::string t, int uid, int id) : text(std::move(t)), userId(ui
     this->id = id; // Feltételezve, hogy az id az ősosztályban van
 }
 
-// Display fájlba írással
+//Olyan mint az operátor
 std::ostream& Comment::display() const {
     os << "Komment (#" << c.userId << "): " << c.text << "\n";
     return os;
@@ -18,7 +18,7 @@ std::string Comment::getType() const {
     return "Komment";
 }
 
-// Feltételezve, hogy ArticleId elérhető (pl. öröklés vagy saját változó)
+
 int Comment::getArticleId() const {
     return ArticleId;
 }

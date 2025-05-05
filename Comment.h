6ded1469baa5320d9de72755f809e8549f67eb0f
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-06 00:30:05 +0200

    Update Comment.h

--- Comment.h
+++ Comment.h
@@ -1,44 +1,3 @@
-///////////////////////////////////////////// Csak Header
-/*
-#ifndef COMMENT_H
-#define COMMENT_H
-
-#include <iostream>
-#include <fstream>
-#include "Content.h"
-#include "Article.h"
-
-class Comment : public Content {
-    std::string text;
-    int userId;
-public:
-    Comment(std::string t, int uid) : text(std::move(t)), userId(uid) {}
-    Comment(std::string t, int uid, int id) : text(std::move(t)), userId(id) {}
-
-    void display() const override {
-        std::ofstream file("comments.txt", std::ios::app); // Fájl megnyitása hozzáfűzési módban
-        if (file.is_open()) {
-            file << *this; // Kiírás fájlba az operátorral
-            file.close();
-        } else {
-            std::cerr << "Nem sikerült megnyitni a fájlt. :(\n";
-        }
-    }
-
-    std::string getType() const override { return "Komment"; }
-
-    friend std::ostream& operator<<(std::ostream& os, const Comment& c) {
-        os << "Komment (#" << c.userId << "): " << c.text << "\n";
-        return os;
-    }
-};
-
-
-    int getArticleId() const {return ArticleId; }
-
-#endif //COMMENT_H
-*/
-///////////////////////////////////////////// Kettészedve
 #ifndef COMMENT_H
 #define COMMENT_H
 
@@ -50,13 +9,13 @@ public:
 class Comment : public Content {
     std::string text;
     int userId;
+    int ArticleId;
 public:
-    Comment(std::string t, int uid);
     Comment(std::string t, int uid, int id);
 
-    std::ostream& display() const override;
-    std::string getType() const override;
-    int getArticleId() const;
+    std::string getType() const override ;
+    int getArticleId() const ;
+    void display() const;
 
     friend std::ostream& operator<<(std::ostream& os, const Comment& c);
 };

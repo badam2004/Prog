c5e7a7c9c860220ebae087562e5f41bbd8a73ae1
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-06 00:31:34 +0200

    Update User.h

--- User.h
+++ User.h
@@ -1,52 +1,6 @@
-/*
 #ifndef USER_H
 #define USER_H
 
-#include <iostream>
-#include <fstream>
-
-enum Role {
-    READER, //0
-    AUTHOR, //1
-    ADMIN   //2
-};
-
-class User {
-    std::string username;
-    Role role; // "reader", "author", "admin"
-public:
-    User(std::string name, std::string r) : username(std::move(name)), role(std::move(r)) {}
-
-    bool canPost() const { return role == Role::AUTHOR || role == Role::ADMIN; }
-    bool canComment() const { return true; }
-    bool canModify() const { return role == Role::ADMIN; ;}
-    bool canDelete() const { return role == Role::ADMIN; ;}
-    void showDashboard() const {
-        std::string user_role;
-        switch (role) {
-            case Role::READER:
-                user_role = "reader";
-            break;
-            case Role::AUTHOR:
-                user_role = "author";
-            break;
-            case Role::ADMIN:
-                user_role = "admin";
-            break;
-
-            default:
-                user_role = "Not defined";
-            break;
-        }
-        std::cout << username << " irányítópultja (" << user_role << ")\n";
-    }
-};
-
-#endif //USER_H
-*/
-/////////////////////////////////////
-#ifndef USER_H
-#define USER_H
 
 #include <string>
 

79643f79c098f56c535ce5942a90f69fd0fba3f4
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-06 00:29:30 +0200

    Update Article.h

--- Article.h
+++ Article.h
@@ -1,31 +1,3 @@
-/*
-#ifndef ARTICLE_H
-#define ARTICLE_H
-
-#include <iostream>
-#include <fstream>
-#include "Content.h"
-
-class Article : public Content {
-    std::string title;
-    std::string content;
-    std::string author;
-    int ArticleId;
-public:
-    Article(std::string t, std::string c, std::string a): title(std::move(t)), content(std::move(c)), author(std::move(a)) {}
-
-    void display() const override {
-        std::cout << "Cikk: " << title << "\nSzerző: " << author << "\n" << content << "\n";
-    }
-
-    std::string getType() const override { return "Cikk"; }
-    std::string getAuthor() const { return author; }
-    int getArticleId() const {return ArticleId; }
-};
-*/
-//////////////////////////////////////////////////
-#endif //ARTICLE_H
-
 #ifndef ARTICLE_H
 #define ARTICLE_H
 

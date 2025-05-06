c390deefeee3a77983aa0ed53984ca9429c9e9e0
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-06 20:14:15 +0200

    Update Content.h

--- Content.h
+++ Content.h
@@ -1,13 +1,1 @@
-#ifndef CONTENT_H
-#define CONTENT_H
 
-#include <iostream>
-
-class Content {
-public:
-    virtual ~Content() = default;
-    virtual void display() const = 0;
-    virtual std::string getType() const = 0;
-};
-
-#endif // CONTENT_H

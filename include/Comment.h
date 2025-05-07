c4bf4238ad3618fac9248cc46b9b5fd19e210b36
badam2004 <adam.janos.bukovics@gmail.com>
2025-05-07 14:54:37 +0200

    Update Comment.h

--- include/Comment.h
+++ include/Comment.h
@@ -6,25 +6,24 @@
 #include "Content.h"
 #include "Article.h"
 
-using std::string, std::ostream, std::move;
 
 class Comment : public Content {
-    string text;       //maga a komment
-    string username;   //felhasználónév
-    string articleTitle;     //az újság Id-ja
+    std::string text;       //maga a komment
+    std::string username;   //felhasznÃ¡lÃ³nÃ©v
+    std::string articleTitle;     //az ÃºjsÃ¡g Id-ja
 public:
-    Comment(string comment_, string username_, string articleTitle_);
+    Comment(std::string comment_, std::string username_, std::string articleTitle_);
 
-    string getText () const { return text; }
-    string getUsername () const { return username; }
-    string getArticleTitle () const {return articleTitle; }
-    string getType() const override { return "Komment"; }
+    std::string getText () const { return text; }
+    std::string getUsername () const { return username; }
+    std::string getArticleTitle () const {return articleTitle; }
+    std::string getType() const override { return "Komment"; }
 
-    void setText (const string& text_) {text = text_;}
-    void setUsername (const string& username_) {username = username_;}
-    void setArticleTitle(const string& articleTitle_) { articleTitle = articleTitle_; }
+    void setText (const std::string& text_) {text = text_;}
+    void setUsername (const std::string& username_) {username = username_;}
+    void setArticleTitle(const std::string& articleTitle_) { articleTitle = articleTitle_; }
     void display() const;
-    friend ostream& operator<<(ostream& os, const Comment& comment_);
+    friend std::ostream& operator<<(std::ostream& os, const Comment& comment_);
 };
 
 #endif // COMMENT_H
